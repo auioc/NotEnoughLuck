5fe469dfb32b59ee15189d55122e6a6e7bcd88ae
WakelessSloth56 <wsloth56@foxmail.com>
2022-01-13 15:46:50 +0800

    build: update to arnicalib 4.1.7

--- src/main/java/org/auioc/mods/notenoughluck/NotEnoughLuck.java
+++ src/main/java/org/auioc/mods/notenoughluck/NotEnoughLuck.java
@@ -1,10 +1,10 @@
 package org.auioc.mods.notenoughluck;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.Marker;
-import org.auioc.mods.arnicalib.api.java.data.Tuple;
 import org.auioc.mods.arnicalib.utils.LogUtil;
-import org.auioc.mods.arnicalib.utils.java.JarUtils;
+import org.auioc.mods.arnicalib.utils.java.VersionUtils;
 import org.auioc.mods.notenoughluck.common.alchemy.PotionRegistry;
 import org.auioc.mods.notenoughluck.common.itemgroup.ItemGroupRegistry;
 import org.auioc.mods.notenoughluck.server.event.ServerEventHandler;
@@ -18,23 +18,25 @@ public final class NotEnoughLuck {
 
     public static final String MOD_ID = "notenoughluck";
     public static final String MOD_NAME = "NotEnoughLuck";
-    public static String MAIN_VERSION;
-    public static String FULL_VERSION;
+    public static final String MAIN_VERSION;
+    public static final String FULL_VERSION;
 
     public static final Logger LOGGER = LogUtil.getLogger(MOD_NAME);
     private static final Marker CORE = LogUtil.getMarker("CORE");
 
     public NotEnoughLuck() {
-        Tuple<String, String> version = JarUtils.getModVersion(getClass());
-        MAIN_VERSION = version.getA();
-        FULL_VERSION = version.getB();
-        LOGGER.info(CORE, "Version: " + MAIN_VERSION + " (" + FULL_VERSION + ")");
-
         final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         final IEventBus forgeEventBus = MinecraftForge.EVENT_BUS;
 
         modSetup(modEventBus);
         forgeSetup(forgeEventBus);
+    }
+
+    static {
+        Pair<String, String> version = VersionUtils.getModVersion(NotEnoughLuck.class);
+        MAIN_VERSION = version.getLeft();
+        FULL_VERSION = version.getRight();
+        LOGGER.info(CORE, "Version: " + MAIN_VERSION + " (" + FULL_VERSION + ")");
     }
 
     private void modSetup(final IEventBus modEventBus) {
