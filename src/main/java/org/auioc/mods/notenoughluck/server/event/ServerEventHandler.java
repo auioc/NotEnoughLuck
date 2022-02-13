44bc27af4d4d3fce4e6879266e86a57448f2d1f9
WakelessSloth56 <wsloth56@foxmail.com>
2022-02-13 21:22:00 +0800

    refactor: use SetEyeOfEnderSurvivableEvent instead of mixin

--- src/main/java/org/auioc/mods/notenoughluck/server/event/ServerEventHandler.java
+++ src/main/java/org/auioc/mods/notenoughluck/server/event/ServerEventHandler.java
@@ -1,8 +1,10 @@
 package org.auioc.mods.notenoughluck.server.event;
 
 import org.auioc.mods.arnicalib.server.event.impl.LivingEatAddEffectEvent;
+import org.auioc.mods.arnicalib.server.event.impl.SetEyeOfEnderSurvivableEvent;
 import org.auioc.mods.notenoughluck.server.command.ServerCommandRegistry;
 import org.auioc.mods.notenoughluck.server.event.handler.EatAddEffectHandler;
+import org.auioc.mods.notenoughluck.server.event.handler.SetEyeOfEnderSurvivableHandler;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
@@ -16,6 +18,11 @@ public class ServerEventHandler {
     @SubscribeEvent
     public static void onLivingEatAddEffect(final LivingEatAddEffectEvent event) {
         EatAddEffectHandler.handle(event);
+    }
+
+    @SubscribeEvent
+    public static void onSetEyeOfEnderSurvivable(final SetEyeOfEnderSurvivableEvent event) {
+        SetEyeOfEnderSurvivableHandler.handle(event);
     }
 
 }
---
+++ src/main/java/org/auioc/mods/notenoughluck/server/event/handler/SetEyeOfEnderSurvivableHandler.java
@@ -0,0 +1,28 @@
+package org.auioc.mods.notenoughluck.server.event.handler;
+
+import org.auioc.mods.arnicalib.server.event.impl.SetEyeOfEnderSurvivableEvent;
+import org.auioc.mods.arnicalib.utils.game.EffectUtils;
+import org.auioc.mods.arnicalib.utils.java.RandomUtils;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.util.Mth;
+import net.minecraft.world.effect.MobEffects;
+
+public class SetEyeOfEnderSurvivableHandler {
+
+    private static final int DEFAULT_CHANCE = 80;
+    private static final int LUCK_MULTIPLIER = 1;
+    private static final int UNLUCK_MULTIPLIER = -4;
+    private static final int BONUS_MULTIPLIER = 4;
+
+    public static void handle(final SetEyeOfEnderSurvivableEvent event) {
+        ServerPlayer player = (ServerPlayer) event.getPlayer();
+
+        int luckBonus = EffectUtils.getEffectLevel(player, MobEffects.LUCK) * LUCK_MULTIPLIER;
+        int unluckBonus = EffectUtils.getEffectLevel(player, MobEffects.UNLUCK) * UNLUCK_MULTIPLIER;
+        int bonus = luckBonus + unluckBonus;
+        int chance = Mth.clamp((DEFAULT_CHANCE + (bonus * BONUS_MULTIPLIER)), 0, 100);
+
+        event.setSurvivable((random) -> RandomUtils.percentageChance(chance, random));
+    }
+
+}
