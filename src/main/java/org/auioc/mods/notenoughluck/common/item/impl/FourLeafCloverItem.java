64103972cacb3ab49b90de3c751cc4ffb4caffa0
WakelessSloth56 <wsloth56@foxmail.com>
2022-02-19 22:07:33 +0800

    feat(item): clover basic functionality
    issue #1

--- src/main/java/org/auioc/mods/notenoughluck/common/item/impl/FourLeafCloverItem.java
+++ src/main/java/org/auioc/mods/notenoughluck/common/item/impl/FourLeafCloverItem.java
@@ -1,9 +1,25 @@
 package org.auioc.mods.notenoughluck.common.item.impl;
 
+import java.time.LocalDate;
+import java.util.function.Predicate;
+import org.auioc.mods.arnicalib.utils.game.ItemUtils;
+import org.auioc.mods.arnicalib.utils.game.PlayerUtils;
 import org.auioc.mods.notenoughluck.common.itemgroup.ItemGroupRegistry;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.UseAnim;
+import net.minecraft.world.level.Level;
 
 public class FourLeafCloverItem extends Item {
+
+    private static final Predicate<ItemStack> IS_PAPER = (stack) -> stack.is(Items.PAPER);
+    private static final int USE_DURATION = 3 * 20;
 
     public FourLeafCloverItem() {
         super(
@@ -11,6 +27,60 @@ public class FourLeafCloverItem extends Item {
                 .tab(ItemGroupRegistry.NELItemGroup)
                 .stacksTo(16)
         );
+    }
+
+    @Override
+    public int getUseDuration(ItemStack itemStack) {
+        return USE_DURATION;
+    }
+
+    @Override
+    public UseAnim getUseAnimation(ItemStack itemStack) {
+        return UseAnim.SPEAR;
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
+        ItemStack stack = player.getItemInHand(hand);
+
+        if (canUse(player)) {
+            player.startUsingItem(hand);
+            return InteractionResultHolder.consume(stack);
+        }
+
+        return InteractionResultHolder.pass(stack);
+    }
+
+
+    @Override
+    public ItemStack finishUsingItem(ItemStack stack, Level level, LivingEntity living) {
+        if (!(living instanceof ServerPlayer)) {
+            return stack;
+        }
+        ServerPlayer player = (ServerPlayer) living;
+
+        if (!canUse(player)) {
+            return stack;
+        }
+
+        int i = lengthOfYear();
+        if (ItemUtils.clearItem(player.getInventory(), IS_PAPER, i, true) != i) {
+            return stack;
+        }
+
+        PlayerUtils.giveItem(player, Items.ACACIA_BOAT);
+
+        stack.shrink(1);
+
+        return stack;
+    }
+
+    private static int lengthOfYear() {
+        return LocalDate.now().lengthOfYear();
+    }
+
+    private static boolean canUse(Player player) {
+        return ItemUtils.countItem(player.getInventory(), IS_PAPER) >= lengthOfYear();
     }
 
 }
