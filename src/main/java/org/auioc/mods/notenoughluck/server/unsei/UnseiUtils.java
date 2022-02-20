83e8abe83dd58395d6f79012d70b307ca0715ecf
WakelessSloth56 <wsloth56@foxmail.com>
2022-02-20 19:20:39 +0800

    feat(unsei): UnseiUtils
    #3

---
+++ src/main/java/org/auioc/mods/notenoughluck/server/unsei/UnseiUtils.java
@@ -0,0 +1,21 @@
+package org.auioc.mods.notenoughluck.server.unsei;
+
+import java.util.Random;
+import org.auioc.mods.arnicalib.utils.game.MCTimeUtils;
+import net.minecraft.server.level.ServerLevel;
+
+public class UnseiUtils {
+
+    public static int getUnseiValue(long seed, int day) {
+        return new Random(seed + day).nextInt(37);
+    }
+
+    public static int getUnseiValue(ServerLevel level) {
+        return getUnseiValue(level.getSeed(), getDay(level.getDayTime()));
+    }
+
+    public static int getDay(long dayTime) {
+        return (((int) (dayTime % 2147483647L)) - MCTimeUtils.ticksAtMidnight + MCTimeUtils.ticksPerDay) / MCTimeUtils.ticksPerDay;
+    }
+
+}
