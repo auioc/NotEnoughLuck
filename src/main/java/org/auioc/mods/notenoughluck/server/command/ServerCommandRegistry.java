3fd8920e1b045de10991c6f44cdc124a134deded
WakelessSloth56 <wsloth56@foxmail.com>
2022-02-21 08:54:52 +0800

    feat: tung shing command

--- src/main/java/org/auioc/mods/notenoughluck/server/command/ServerCommandRegistry.java
+++ src/main/java/org/auioc/mods/notenoughluck/server/command/ServerCommandRegistry.java
@@ -2,17 +2,22 @@ package org.auioc.mods.notenoughluck.server.command;
 
 import static net.minecraft.commands.Commands.literal;
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.tree.CommandNode;
 import org.auioc.mods.arnicalib.server.command.impl.VersionCommand;
 import org.auioc.mods.notenoughluck.NotEnoughLuck;
+import org.auioc.mods.notenoughluck.server.command.impl.TungShingCommand;
 import net.minecraft.commands.CommandSourceStack;
 
 public class ServerCommandRegistry {
 
+    public static final CommandNode<CommandSourceStack> NODE = literal(NotEnoughLuck.MOD_ID).build();
+
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
-        org.auioc.mods.arnicalib.server.command.ServerCommandRegistry.getRootNode(dispatcher).addChild(
-            literal(NotEnoughLuck.MOD_ID).executes((ctx) -> VersionCommand.getModVersion(ctx, NotEnoughLuck.MAIN_VERSION, NotEnoughLuck.FULL_VERSION, NotEnoughLuck.MOD_NAME))
-                .build()
-        );
+        NODE.addChild(literal("version").executes((ctx) -> VersionCommand.getModVersion(ctx, NotEnoughLuck.MAIN_VERSION, NotEnoughLuck.FULL_VERSION, NotEnoughLuck.MOD_NAME)).build());
+
+        NODE.addChild(TungShingCommand.NODE);
+
+        org.auioc.mods.arnicalib.server.command.ServerCommandRegistry.getRootNode(dispatcher).addChild(NODE);
     }
 
 }
---
+++ src/main/java/org/auioc/mods/notenoughluck/server/command/impl/TungShingCommand.java
@@ -0,0 +1,33 @@
+package org.auioc.mods.notenoughluck.server.command.impl;
+
+import static net.minecraft.commands.Commands.argument;
+import static net.minecraft.commands.Commands.literal;
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.tree.CommandNode;
+import org.auioc.mods.notenoughluck.utils.UnseiUtils;
+import net.minecraft.commands.CommandSourceStack;
+
+public class TungShingCommand {
+
+    public static final CommandNode<CommandSourceStack> NODE =
+        literal("tungShing")
+            .requires(source -> source.hasPermission(3))
+            .then(
+                literal("open")
+                    .executes((ctx) -> open(ctx, UnseiUtils.getDay(ctx.getSource().getLevel().getDayTime())))
+                    .then(
+                        argument("day", IntegerArgumentType.integer(0))
+                            .executes((ctx) -> open(ctx, IntegerArgumentType.getInteger(ctx, "day")))
+                    )
+            )
+            .build();
+
+    private static int open(CommandContext<CommandSourceStack> ctx, int day) throws CommandSyntaxException {
+        UnseiUtils.sendUpdateTungShingPacket(ctx.getSource().getPlayerOrException(), ctx.getSource().getLevel().getSeed(), day);
+
+        return Command.SINGLE_SUCCESS;
+    }
+}
