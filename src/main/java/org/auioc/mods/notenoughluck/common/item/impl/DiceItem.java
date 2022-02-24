cba01e2a836a449e68bf6fedce6af8dac6aef228
WakelessSloth56 <wsloth56@foxmail.com>
2022-02-25 00:22:28 +0800

    feat(item): show pips on dice  model
    #6

--- src/main/java/org/auioc/mods/notenoughluck/common/item/impl/DiceItem.java
+++ src/main/java/org/auioc/mods/notenoughluck/common/item/impl/DiceItem.java
@@ -1,9 +1,15 @@
 package org.auioc.mods.notenoughluck.common.item.impl;
 
+import java.util.function.Consumer;
+import org.auioc.mods.notenoughluck.client.renderer.DiceItemRenderer;
 import org.auioc.mods.notenoughluck.common.itemgroup.NELItemGroups;
+import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.IItemRenderProperties;
 
 public class DiceItem extends Item {
 
@@ -19,6 +25,17 @@ public class DiceItem extends Item {
     public boolean onDroppedByPlayer(ItemStack stack, Player player) {
         stack.getOrCreateTag().putInt("pips", player.getRandom().nextInt(1, 7));
         return true;
+    }
+
+    @Override
+    @OnlyIn(Dist.CLIENT)
+    public void initializeClient(Consumer<IItemRenderProperties> consumer) {
+        consumer.accept(new IItemRenderProperties() {
+            @Override
+            public BlockEntityWithoutLevelRenderer getItemStackRenderer() {
+                return DiceItemRenderer.INSTANCE;
+            }
+        });
     }
 
 }
