35a97d659d0b95a809346b6b791a0577bc47a3a4
WakelessSloth56 <wsloth56@foxmail.com>
2022-02-26 14:21:06 +0800

    feat(item): change tung shing item to block item
    #2, #9

--- src/main/java/org/auioc/mods/notenoughluck/common/item/impl/TungShingItem.java
+++ src/main/java/org/auioc/mods/notenoughluck/common/item/impl/TungShingItem.java
@@ -1,23 +1,26 @@
 package org.auioc.mods.notenoughluck.common.item.impl;
 
 import org.auioc.mods.notenoughluck.client.gui.screen.tungshing.TungShingScreenUtils;
+import org.auioc.mods.notenoughluck.common.block.NELBlocks;
 import org.auioc.mods.notenoughluck.common.item.NELItems;
 import org.auioc.mods.notenoughluck.common.itemgroup.NELItemGroups;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.fml.DistExecutor;
 
-public class TungShingItem extends Item {
+public class TungShingItem extends BlockItem {
 
     public static final int COOLDOWN = 10 * 20;
 
     public TungShingItem() {
         super(
+            NELBlocks.TUNG_SHING_BLOCK.get(),
             new Item.Properties()
                 .tab(NELItemGroups.NELItemGroup)
                 .stacksTo(1)
