919049fe446cca1ee56a67c236da5c929c480e12
WakelessSloth56 <wsloth56@foxmail.com>
2022-02-27 16:33:21 +0800

    feat(mixin): player attack luck bonus
    close #10

--- src/main/java/org/auioc/mods/notenoughluck/mixin/server/MixinPlayer.java
+++ src/main/java/org/auioc/mods/notenoughluck/mixin/server/MixinPlayer.java
@@ -1,6 +1,11 @@
 package org.auioc.mods.notenoughluck.mixin.server;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+import org.apache.commons.lang3.tuple.Triple;
 import org.auioc.mods.arnicalib.utils.game.EffectUtils;
+import org.auioc.mods.arnicalib.utils.java.RandomUtils;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.At.Shift;
@@ -26,6 +31,17 @@ public abstract class MixinPlayer {
     //     ci.cancel();
     // }
 
+    private static final List<Triple<Integer, Function<Integer, Float>, Function<Integer, Float>>> ATTACK_LUCK_BONUS_MAP =
+        new ArrayList<Triple<Integer, Function<Integer, Float>, Function<Integer, Float>>>() {
+            {
+                add(Triple.of(15, (n) -> 1.25F, (n) -> 1.0F));
+                add(Triple.of(30, (n) -> 1.5F, (n) -> 1.0F));
+                add(Triple.of(45, (n) -> 1.75F, (n) -> 1.0F));
+                add(Triple.of(60, (n) -> 2.0F, (n) -> 1.0F));
+                add(Triple.of(75, (n) -> 1.0F + 0.2F * ((float) n), (n) -> 0.75F + ((float) Math.pow(1.5D, ((double) n) - 4.0D))));
+            }
+        };
+
     @ModifyVariable(
         method = "Lnet/minecraft/world/entity/player/Player;attack(Lnet/minecraft/world/entity/Entity;)V",
         at = @At(
@@ -48,6 +64,18 @@ public abstract class MixinPlayer {
         int unluck = EffectUtils.getEffectLevel(player, MobEffects.UNLUCK);
         if (unluck > 0 && !(player.getRandom().nextDouble() < Math.pow(0.8, (double) unluck))) {
             return 0.0F;
+        }
+
+        int luck = EffectUtils.getEffectLevel(player, MobEffects.LUCK);
+        if (luck > 0) {
+            Triple<Integer, Function<Integer, Float>, Function<Integer, Float>> bonusTriple =
+                ATTACK_LUCK_BONUS_MAP.get(Math.min(luck - 1, ATTACK_LUCK_BONUS_MAP.size() - 1));
+
+            if (RandomUtils.percentageChance(bonusTriple.getLeft(), player.getRandom())) {
+                f *= bonusTriple.getMiddle().apply(luck);
+            } else {
+                f *= bonusTriple.getRight().apply(luck);
+            }
         }
 
         return f;
