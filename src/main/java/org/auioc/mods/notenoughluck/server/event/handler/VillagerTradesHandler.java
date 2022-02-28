d080ed98438c3e86ce8d5a6d02e7f17b3076bd40
WakelessSloth56 <wsloth56@foxmail.com>
2022-02-28 15:00:34 +0800

    feat(event): listen to VillagerTradesEvent
    #14

---
+++ src/main/java/org/auioc/mods/notenoughluck/server/event/handler/VillagerTradesHandler.java
@@ -0,0 +1,9 @@
+package org.auioc.mods.notenoughluck.server.event.handler;
+
+import net.minecraftforge.event.village.VillagerTradesEvent;
+
+public class VillagerTradesHandler {
+
+    public static void handle(final VillagerTradesEvent event) {}
+
+}
