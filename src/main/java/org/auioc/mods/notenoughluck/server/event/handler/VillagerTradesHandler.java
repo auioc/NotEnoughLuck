a51c2de44a1120e1ddd2e7712bd07306558f99d9
WakelessSloth56 <wsloth56@foxmail.com>
2022-02-28 15:11:04 +0800

    feat: add indulgence to cleric lv.5 trades
    #14

--- src/main/java/org/auioc/mods/notenoughluck/server/event/handler/VillagerTradesHandler.java
+++ src/main/java/org/auioc/mods/notenoughluck/server/event/handler/VillagerTradesHandler.java
@@ -1,9 +1,32 @@
 package org.auioc.mods.notenoughluck.server.event.handler;
 
+import org.auioc.mods.notenoughluck.common.item.NELItems;
+import net.minecraft.world.entity.npc.VillagerProfession;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraftforge.common.BasicItemListing;
 import net.minecraftforge.event.village.VillagerTradesEvent;
 
 public class VillagerTradesHandler {
 
-    public static void handle(final VillagerTradesEvent event) {}
+    private static final int EMERALD_COST = 32;
+    private static final int SALE_COUNT = 1;
+    private static final int MAX_TRADES = 2;
+    private static final int VILLAGER_XP = 30;
+    private static final float PRICE_MULTIPLIER = 0.25F;
+
+    public static void handle(final VillagerTradesEvent event) {
+        if (event.getType() == VillagerProfession.CLERIC) {
+            event.getTrades().get(5).add(
+                new BasicItemListing(
+                    new ItemStack(Items.EMERALD, EMERALD_COST),
+                    new ItemStack(NELItems.INDULGENCE_ITEM.get(), SALE_COUNT),
+                    MAX_TRADES,
+                    VILLAGER_XP,
+                    PRICE_MULTIPLIER
+                )
+            );
+        }
+    }
 
 }
