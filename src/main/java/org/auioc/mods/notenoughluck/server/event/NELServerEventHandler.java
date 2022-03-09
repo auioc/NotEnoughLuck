745cbde402ba3580b903e15bbf3ca0f0a32bb90f
WakelessSloth56 <wsloth56@foxmail.com>
2022-03-09 15:58:47 +0800

    feat(unsei): set player base unsei value on login
    #3

--- src/main/java/org/auioc/mods/notenoughluck/server/event/NELServerEventHandler.java
+++ src/main/java/org/auioc/mods/notenoughluck/server/event/NELServerEventHandler.java
@@ -4,10 +4,12 @@ import org.auioc.mods.arnicalib.server.event.impl.LivingEatAddEffectEvent;
 import org.auioc.mods.arnicalib.server.event.impl.SetEyeOfEnderSurvivableEvent;
 import org.auioc.mods.notenoughluck.server.command.NELServerCommands;
 import org.auioc.mods.notenoughluck.server.event.handler.EatAddEffectHandler;
+import org.auioc.mods.notenoughluck.server.event.handler.PlayerLoginHandler;
 import org.auioc.mods.notenoughluck.server.event.handler.SetEyeOfEnderSurvivableHandler;
 import org.auioc.mods.notenoughluck.server.event.handler.VillagerTradesHandler;
 import org.auioc.mods.notenoughluck.server.event.handler.WandererTradesHandler;
 import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.event.village.VillagerTradesEvent;
 import net.minecraftforge.event.village.WandererTradesEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -37,6 +39,11 @@ public class NELServerEventHandler {
     @SubscribeEvent
     public static void onSetEyeOfEnderSurvivable(final SetEyeOfEnderSurvivableEvent event) {
         SetEyeOfEnderSurvivableHandler.handle(event);
+    }
+
+    @SubscribeEvent
+    public static void onPlayerLogin(final PlayerEvent.PlayerLoggedInEvent event) {
+        PlayerLoginHandler.handle(event);
     }
 
 }
