ec80d478a8c44d65ae7d7b03aa6ba97fc8274a52
WakelessSloth56 <wsloth56@foxmail.com>
2022-04-07 18:53:26 +0800

    feat(integ): ChampionHelper

--- src/main/java/org/auioc/mcmod/notenoughluck/integration/champions/affix/impl/JinxAffix.java
+++ src/main/java/org/auioc/mcmod/notenoughluck/integration/champions/affix/impl/JinxAffix.java
@@ -2,6 +2,7 @@ package org.auioc.mcmod.notenoughluck.integration.champions.affix.impl;
 
 import java.util.function.ToIntFunction;
 import org.auioc.mcmod.arnicalib.utils.java.RandomUtils;
+import org.auioc.mcmod.notenoughluck.integration.champions.ChampionHelper;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
@@ -10,7 +11,6 @@ import top.theillusivec4.champions.api.AffixCategory;
 import top.theillusivec4.champions.api.IChampion;
 import top.theillusivec4.champions.common.affix.core.BasicAffix;
 import top.theillusivec4.champions.common.rank.Rank;
-import top.theillusivec4.champions.common.rank.RankManager;
 
 public class JinxAffix extends BasicAffix {
 
@@ -24,12 +24,11 @@ public class JinxAffix extends BasicAffix {
     @Override
     public float onDamage(IChampion champion, DamageSource source, float amount, float newAmount) {
         if (source.getEntity() instanceof LivingEntity) {
-            var rank = champion.getServer().getRank().orElse(RankManager.getLowestRank());
             ((LivingEntity) source.getEntity()).addEffect(
                 new MobEffectInstance(
                     MobEffects.UNLUCK,
                     EFFECT_DURATION,
-                    EFFECT_AMPLIFIER.applyAsInt(rank)
+                    EFFECT_AMPLIFIER.applyAsInt(ChampionHelper.getRank(champion))
                 )
             );
         }
