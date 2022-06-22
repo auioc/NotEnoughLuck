05e55f9908616c3a2143e7b6a25ce247b1a03ae8
lainio24 <lainio24@outlook.com>
2022-06-22 09:40:28 +0800

    feat(item): use arnicalib 5.3.2 new features
    UsableItem, EffectUtils.makeIncurable()

--- src/main/java/org/auioc/mcmod/notenoughluck/common/item/impl/FourLeafCloverItem.java
+++ src/main/java/org/auioc/mcmod/notenoughluck/common/item/impl/FourLeafCloverItem.java
@@ -2,13 +2,13 @@ package org.auioc.mcmod.notenoughluck.common.item.impl;
 
 import java.time.LocalDate;
 import java.util.function.Predicate;
+import org.auioc.mcmod.arnicalib.api.game.item.UsableItem;
 import org.auioc.mcmod.arnicalib.utils.game.ContainerUtils;
 import org.auioc.mcmod.arnicalib.utils.game.PlayerUtils;
 import org.auioc.mcmod.notenoughluck.common.item.NELItems;
 import org.auioc.mcmod.notenoughluck.common.itemgroup.NELItemGroups;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
@@ -17,7 +17,7 @@ import net.minecraft.world.item.Items;
 import net.minecraft.world.item.UseAnim;
 import net.minecraft.world.level.Level;
 
-public class FourLeafCloverItem extends Item {
+public class FourLeafCloverItem extends UsableItem {
 
     private static final Predicate<ItemStack> IS_PAPER = (stack) -> stack.is(Items.PAPER);
     private static final int USE_DURATION = 3 * 20;
@@ -26,43 +26,19 @@ public class FourLeafCloverItem extends Item {
         super(
             new Item.Properties()
                 .tab(NELItemGroups.NELItemGroup)
-                .stacksTo(16)
+                .stacksTo(16),
+            USE_DURATION,
+            UseAnim.BOW
         );
     }
 
     @Override
-    public int getUseDuration(ItemStack itemStack) {
-        return USE_DURATION;
-    }
-
-    @Override
-    public UseAnim getUseAnimation(ItemStack itemStack) {
-        return UseAnim.BOW;
-    }
-
-    @Override
-    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
-        ItemStack stack = player.getItemInHand(hand);
-
-        if (canUse(player)) {
-            player.startUsingItem(hand);
-            return InteractionResultHolder.consume(stack);
-        }
-
-        return InteractionResultHolder.pass(stack);
-    }
-
-
-    @Override
     public ItemStack finishUsingItem(ItemStack stack, Level level, LivingEntity living) {
-        if (!(living instanceof ServerPlayer)) {
-            return stack;
-        }
+        if (!(living instanceof ServerPlayer)) return stack;
+
         var player = (ServerPlayer) living;
 
-        if (!canUse(player)) {
-            return stack;
-        }
+        if (!canUse(player)) return stack;
 
         if (!player.getAbilities().instabuild) {
             int i = lengthOfYear();
@@ -76,6 +52,11 @@ public class FourLeafCloverItem extends Item {
         PlayerUtils.giveItem(player, NELItems.TUNG_SHING_ITEM.get());
 
         return stack;
+    }
+
+    @Override
+    public boolean canUse(Level level, Player player, InteractionHand hand, ItemStack itemStack) {
+        return canUse(player);
     }
 
     private static int lengthOfYear() {
--- src/main/java/org/auioc/mcmod/notenoughluck/common/item/impl/IndulgenceItem.java
+++ src/main/java/org/auioc/mcmod/notenoughluck/common/item/impl/IndulgenceItem.java
@@ -1,21 +1,19 @@
 package org.auioc.mcmod.notenoughluck.common.item.impl;
 
-import java.util.ArrayList;
+import org.auioc.mcmod.arnicalib.api.game.item.UsableItem;
+import org.auioc.mcmod.arnicalib.utils.game.EffectUtils;
 import org.auioc.mcmod.notenoughluck.common.effect.NELMobEffects;
 import org.auioc.mcmod.notenoughluck.common.itemgroup.NELItemGroups;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.UseAnim;
 import net.minecraft.world.level.Level;
 
-public class IndulgenceItem extends Item {
+public class IndulgenceItem extends UsableItem {
 
     private static final int USE_DURATION = 3 * 20;
     private static final int REDEMPTION_DURATION = 20 * 60 * 20;
@@ -25,32 +23,20 @@ public class IndulgenceItem extends Item {
             new Item.Properties()
                 .tab(NELItemGroups.NELItemGroup)
                 .rarity(Rarity.RARE)
-                .stacksTo(1)
+                .stacksTo(1),
+            USE_DURATION,
+            UseAnim.BOW
         );
-    }
-
-    @Override
-    public int getUseDuration(ItemStack itemStack) {
-        return USE_DURATION;
-    }
-
-    @Override
-    public UseAnim getUseAnimation(ItemStack itemStack) {
-        return UseAnim.BOW;
-    }
-
-    @Override
-    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
-        player.startUsingItem(hand);
-        return InteractionResultHolder.consume(player.getItemInHand(hand));
     }
 
     @Override
     public ItemStack finishUsingItem(ItemStack stack, Level level, LivingEntity living) {
         if (living instanceof ServerPlayer player) {
-            var redemptionEffect = new MobEffectInstance(NELMobEffects.REDEMPTION.get(), REDEMPTION_DURATION);
-            redemptionEffect.setCurativeItems(new ArrayList<ItemStack>());
-            player.addEffect(redemptionEffect);
+            player.addEffect(
+                EffectUtils.makeIncurable(
+                    new MobEffectInstance(NELMobEffects.REDEMPTION.get(), REDEMPTION_DURATION)
+                )
+            );
             if (!player.getAbilities().instabuild) {
                 stack.shrink(1);
             }
