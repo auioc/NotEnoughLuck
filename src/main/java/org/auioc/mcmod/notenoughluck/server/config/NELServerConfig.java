7ba3b0aa28af3a46d867aad138608d55b7d8ad3a
lainio24 <lainio24@outlook.com>
2022-06-24 10:01:47 +0800

    feat(unsei): configurable unsei effect interval

--- src/main/java/org/auioc/mcmod/notenoughluck/server/config/NELServerConfig.java
+++ src/main/java/org/auioc/mcmod/notenoughluck/server/config/NELServerConfig.java
@@ -1,15 +1,29 @@
 package org.auioc.mcmod.notenoughluck.server.config;
 
 import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.common.ForgeConfigSpec.IntValue;
 
 public class NELServerConfig {
 
     public static final ForgeConfigSpec CONFIG;
 
+    public static final IntValue UnseiEffectInterval;
+
     static {
         ForgeConfigSpec.Builder b = new ForgeConfigSpec.Builder();
 
+        {
+            b.push("unsei");
+            {
+                b.push("effect");
+                UnseiEffectInterval = b.defineInRange("interval", 120, 1, Integer.MAX_VALUE);
+                b.pop();
+            }
+            b.pop();
+        }
+
         CONFIG = b.build();
     }
+
 
 }
