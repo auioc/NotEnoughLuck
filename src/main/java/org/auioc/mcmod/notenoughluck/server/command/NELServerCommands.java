af87c5a78b22aa4f0e6df73b7668951610fa8a4f
WakelessSloth56 <wsloth56@foxmail.com>
2022-08-15 09:29:44 +0800

    feat: nel command alias

--- src/main/java/org/auioc/mcmod/notenoughluck/server/command/NELServerCommands.java
+++ src/main/java/org/auioc/mcmod/notenoughluck/server/command/NELServerCommands.java
@@ -1,13 +1,13 @@
 package org.auioc.mcmod.notenoughluck.server.command;
 
 import static net.minecraft.commands.Commands.literal;
-import com.mojang.brigadier.CommandDispatcher;
-import com.mojang.brigadier.tree.CommandNode;
 import org.auioc.mcmod.arnicalib.common.command.impl.VersionCommand;
 import org.auioc.mcmod.arnicalib.server.command.AHServerCommands;
 import org.auioc.mcmod.notenoughluck.NotEnoughLuck;
 import org.auioc.mcmod.notenoughluck.server.command.impl.TungShingCommand;
 import org.auioc.mcmod.notenoughluck.server.command.impl.UnseiCommand;
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.tree.CommandNode;
 import net.minecraft.commands.CommandSourceStack;
 
 public class NELServerCommands {
@@ -21,6 +21,7 @@ public class NELServerCommands {
         NODE.addChild(TungShingCommand.NODE);
 
         AHServerCommands.getAHNode(dispatcher).addChild(NODE);
+        dispatcher.register(literal("nel").redirect(NODE));
     }
 
 }
