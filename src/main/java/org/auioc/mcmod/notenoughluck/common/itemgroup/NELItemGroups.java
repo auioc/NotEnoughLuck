a5674d8ecf24fa91116a522ed21a8e2285e0b2ba
WakelessSloth56 <wsloth56@foxmail.com>
2022-10-08 22:37:48 +0800

    feat: remove potions item group

--- src/main/java/org/auioc/mcmod/notenoughluck/common/itemgroup/NELItemGroups.java
+++ src/main/java/org/auioc/mcmod/notenoughluck/common/itemgroup/NELItemGroups.java
@@ -1,14 +1,12 @@
 package org.auioc.mcmod.notenoughluck.common.itemgroup;
 
 import org.auioc.mcmod.notenoughluck.common.itemgroup.impl.NELItemGroup;
-import org.auioc.mcmod.notenoughluck.common.itemgroup.impl.NELPotionGroup;
 import net.minecraft.world.item.CreativeModeTab;
 
 public class NELItemGroups {
 
     public static void init() {}
 
-    public static CreativeModeTab NELItemGroup = new NELItemGroup();
-    public static CreativeModeTab NELPotionGroup = new NELPotionGroup();
+    public static final CreativeModeTab NELItemGroup = new NELItemGroup();
 
 }
